e1b4d65c0253ea9b6bfb44720f8f3b8e10ac295a
HorizonT9 <losthorizon942@gmail.com>
2018-09-06 11:02:43 +0800

    增加文件保存

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"os"
 	"test/hservice"
 	"topMovieCr/movie"
 )
@@ -27,18 +31,32 @@ func main() {
 	}
 
 	movies = movie.GetMovie()
-	for _, v := range movies {
-		prepare, preerr := doubanDB.Prepare("insert into top_movie_copy1(rank,title,foreititle,othertitle,rating_num,people_info,on_time,country,tag,info,img) values (?,?,?,?,?,?,?,?,?,?,?)")
-		if preerr != nil {
-			log.Print("Insert Error:", preerr)
-			return
-		}
-		_, execerr := prepare.Exec(v.Rank, v.Title, v.ForeiTitle, v.OtherTitle, v.RatingNum, v.People, v.Time, v.Country, v.Tag, v.Info, v.Img)
-		if execerr != nil {
-			log.Print("Exec Error:", execerr.Error())
-			return
-		}
+	//sql保存
+	//for _, v := range movies {
+	//	prepare, preerr := doubanDB.Prepare("insert into top_movie_copy1(rank,title,foreititle,othertitle,rating_num,people_info,on_time,country,tag,info,img) values (?,?,?,?,?,?,?,?,?,?,?)")
+	//	if preerr != nil {
+	//		log.Print("Insert Error:", preerr)
+	//		return
+	//	}
+	//	_, execerr := prepare.Exec(v.Rank, v.Title, v.ForeiTitle, v.OtherTitle, v.RatingNum, v.People, v.Time, v.Country, v.Tag, v.Info, v.Img)
+	//	if execerr != nil {
+	//		log.Print("Exec Error:", execerr.Error())
+	//		return
+	//	}
+	//}
+
+	//文件保存
+	jsondata, jsonerr := json.Marshal(movies)
+	if jsonerr != nil {
+		panic(jsonerr)
 	}
+	fmt.Printf("%s\n", jsondata)
+	fileObj, err := os.OpenFile("topmovie.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Failed to open the file", err.Error())
+		os.Exit(2)
+	}
+	io.WriteString(fileObj, string(jsondata))
 
 	log.Println("Done")
 }
